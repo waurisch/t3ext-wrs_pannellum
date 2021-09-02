ec7cdd4aa975ee3ce19a7cb5a40d4f8ceb9eee33
Thomas Scholze <thomas.scholze@gmail.com>
2021-09-02 12:52:55 +0200

    [TASK] remove support for TYPO8 v8 LTS and allow installation up to TYPO3 v11 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-          'typo3' => '8.7.13-9.5.99',
+          'typo3' => '9.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
